365f767c68bfeacca8eb1a57ba714e0603aab371
Aday <adymr.gp@gmail.com>
2024-12-12 23:19:50 +0100

    model deployment (not finished)

--- project1/database/migrations/2024_12_12_125035_create_albums_table.php
+++ project1/database/migrations/2024_12_12_125035_create_albums_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('cover');
+            $table->date('release_date');
             $table->timestamps();
         });
     }
--- project1/database/migrations/2024_12_12_125116_create_artists_table.php
+++ project1/database/migrations/2024_12_12_125116_create_artists_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('index_date');
+            $table->string('cover');
             $table->timestamps();
         });
     }
